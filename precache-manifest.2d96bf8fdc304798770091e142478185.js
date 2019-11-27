3322a0feb18473bad5d515e1728d50dbf487d061
tythia <lrsong_2008@163.com>
2019-11-27 12:20:28 +0000

    Deploying to gh-pages from master 9462b2008ed8f4aae115dc918e720cdc91ed3475

---
+++ precache-manifest.2d96bf8fdc304798770091e142478185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c384e79283dfef1f7b14eb1c2e8dfd15",
+    "url": "/my-react-app-demo/index.html"
+  },
+  {
+    "revision": "fc08db52ac6188eb5ad2",
+    "url": "/my-react-app-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "275a9cd351e9e7e11e3f",
+    "url": "/my-react-app-demo/static/js/2.3d9806eb.chunk.js"
+  },
+  {
+    "revision": "fc08db52ac6188eb5ad2",
+    "url": "/my-react-app-demo/static/js/main.24457be5.chunk.js"
+  },
+  {
+    "revision": "a76985b34db9b410c8aa",
+    "url": "/my-react-app-demo/static/js/runtime-main.02e8c629.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-react-app-demo/static/media/logo.25bf045c.svg"
+  }
+]);
